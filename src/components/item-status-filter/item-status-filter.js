e5d50f7d24f4e9f9c7c9457689c2d6c3fd8cfe4e
Alesia Luzko <lesya.grishko@gmail.com>
2023-11-23 17:43:13 +0100

    ItemStatusFilter: replace the class component to functional component

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,35 +1,32 @@
-import React, {Component} from "react";
+import React from "react";
 
-export default class ItemStatusFilter extends Component {
+const ItemStatusFilter = ({filter, onFilterChange}) => {
 
-  buttons = [
+  const buttons = [
     {name: 'all', label: 'All'},
     {name: 'active', label: 'Active'},
     {name: 'done', label: 'Done'}
   ];
 
-  render() {
-
-    const {filter, onFilterChange} = this.props;
-
-    const buttons = this.buttons.map(({name, label}) => {
-      const isActive = filter === name;
-      const clazz = isActive ? 'btn-primary' : 'btn-outline-primary';
-
-      return (
-        <button type="button"
-                className={`btn ${clazz}`}
-                key={name}
-                onClick={() => onFilterChange(name)}>
-          {label}
-        </button>
-      );
-    });
+  const button = buttons.map(({name, label}) => {
+    const isActive = filter === name;
+    const clazz = isActive ? 'btn-primary' : 'btn-outline-primary';
 
     return (
-      <div className="btn-group">
-        {buttons}
-      </div>
+      <button type="button"
+              className={`btn ${clazz}`}
+              key={name}
+              onClick={() => onFilterChange(name)}>
+        {label}
+      </button>
     );
-  }
-}
+  });
+
+  return (
+    <div className="btn-group">
+      {button}
+    </div>
+  );
+};
+
+export default ItemStatusFilter;
